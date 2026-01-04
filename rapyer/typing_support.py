2c64bfd685690cc4deb2b3d87aa7918c72b33066
yedidyakfir <yedidyakfir@gmail.com>
2026-01-04 21:47:14 +0200

    [fill-coverage] - ignore support for python 3.10 and 3.13 for coverage check

--- rapyer/typing_support.py
+++ rapyer/typing_support.py
@@ -1,8 +1,8 @@
 # For python 3.10 support
 try:
     from typing import Self, Unpack
-except ImportError:
-    from typing_extensions import Self, Unpack
+except ImportError:  # pragma: no cover
+    from typing_extensions import Self, Unpack  # pragma: no cover
 
 # For python 3.13 support
 try:
