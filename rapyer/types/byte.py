2fbf42faa53c620a186e8a78a8698eb95aac4832
yedidyakfir <yedidyakfir@gmail.com>
2026-01-04 22:01:23 +0200

    [fill-coverage] - fix pipeline action, serialize before adding to redis

--- rapyer/types/byte.py
+++ rapyer/types/byte.py
@@ -14,7 +14,9 @@ class RedisBytes(bytes, RedisType):
     def __iadd__(self, other):
         new_value = self + other
         if self.pipeline:
-            self.pipeline.json().set(self.key, self.json_path, new_value)
+            self.pipeline.json().set(
+                self.key, self.json_path, self.serialize_unknown(new_value)
+            )
         return self.__class__(new_value)
 
     @classmethod
