da9c3183596e13987e00b7386627fdce3cc659f4
yedidyakfir <yedidyakfir@gmail.com>
2026-01-06 22:05:53 +0200

    [fill-coverage] - redundant test

--- tests/integration/functioninality/test_acreate_index.py
+++ tests/integration/functioninality/test_acreate_index.py
@@ -17,13 +17,6 @@ async def test_acreate_index_raises_unsupported_filter_field_error_for_unsupport
 
 
 @pytest.mark.asyncio
-async def test_acreate_index_raises_unsupported_indexed_field_error_for_unsupported_type_sanity():
-    # Act & Assert
-    with pytest.raises(UnsupportedIndexedFieldError):
-        await UnsupportedIndexModel.acreate_index()
-
-
-@pytest.mark.asyncio
 @pytest.mark.parametrize(
     ["model_class"],
     [
