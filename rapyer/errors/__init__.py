3989bb9010e7a9ed14da5d06deeefac93fad9cdd
yedidyakfir <yedidyakfir@gmail.com>
2026-01-21 12:39:44 +0200

    [176-add-bulk-key-support-to-afind-for-optimized-multi-key-retrieval] - import error from rapyer.errors

--- rapyer/errors/__init__.py
+++ rapyer/errors/__init__.py
@@ -5,6 +5,7 @@ from rapyer.errors.base import (
     RapyerError,
     ScriptsNotInitializedError,
     UnsupportedIndexedFieldError,
+    RapyerModelDoesntExist,
 )
 
 __all__ = [
@@ -14,4 +15,5 @@ __all__ = [
     "RapyerError",
     "ScriptsNotInitializedError",
     "UnsupportedIndexedFieldError",
+    "RapyerModelDoesntExist",
 ]
