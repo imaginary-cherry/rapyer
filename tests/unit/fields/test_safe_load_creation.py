a6c83bc2c29a8db4da07a4bdf9c383650cb7c417
yedidyakfir <yedidyakfir@gmail.com>
2026-02-17 10:57:38 +0200

    [200-implement-afind_one-function-for-single-result-filter-queries] - optimize imports

--- tests/unit/fields/test_safe_load_creation.py
+++ tests/unit/fields/test_safe_load_creation.py
@@ -1,7 +1,6 @@
 from typing import Annotated, get_args, get_origin
 
 import pytest
-
 from rapyer.fields.safe_load import SafeLoad, SafeLoadAnnotation
 from tests.models.safe_load_types import (
     ModelWithMixedListFields,
