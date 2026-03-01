31fae2df38dece03c707520dd0009cb5ea21e5e4
yedidyakfir <yedidyakfir@gmail.com>
2026-03-01 14:34:05 +0200

    optimize

--- tests/integration/pipeline/test__update_method_dont_set_redis.py
+++ tests/integration/pipeline/test__update_method_dont_set_redis.py
@@ -6,7 +6,6 @@ import rapyer
 from tests.models.collection_types import ComprehensiveTestModel
 from tests.models.simple_types import (
     FloatModel,
-    BytesModel,
     DatetimeModel,
     DatetimeTimestampModel,
 )
