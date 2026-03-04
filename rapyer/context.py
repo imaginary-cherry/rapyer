caa78c1ebf6ccfed7569565340a3c8ffccb3e066
YedidyaHKfir <Yedidya@dono.ai>
2026-03-04 21:59:07 +0200

    [216-add-use_existing-flag-to-apipeline] - fix pipe context

--- rapyer/context.py
+++ rapyer/context.py
@@ -2,18 +2,18 @@ import contextlib
 import contextvars
 from typing import Optional
 
-from redis.asyncio.client import Redis
+from redis.client import Pipeline
 
 # Create a context variable to store the context
-_context_pipe: contextvars.ContextVar[Optional["Redis"]] = contextvars.ContextVar(
+_context_pipe: contextvars.ContextVar[Optional["Pipeline"]] = contextvars.ContextVar(
     "redis", default=None
 )
 
 
 @contextlib.contextmanager
-def with_pipe_context(pipe: Redis):
+def with_pipe_context(pipe: Pipeline):
+    pipe_prev = _context_pipe.set(pipe)
     try:
-        pipe_prev = _context_pipe.set(pipe)
         yield pipe
     finally:
         _context_pipe.reset(pipe_prev)
