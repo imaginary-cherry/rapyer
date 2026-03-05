48a6e9f4ece83a791426e873b6d5c037d51c9e80
YedidyaHKfir <Yedidya@dono.ai>
2026-03-05 17:05:01 +0200

    black

--- rapyer/types/float.py
+++ rapyer/types/float.py
@@ -1,7 +1,13 @@
 from typing import TYPE_CHECKING, TypeAlias
 
-from rapyer.scripts import (NUM_FLOORDIV_SCRIPT_NAME, NUM_MOD_SCRIPT_NAME, NUM_MUL_SCRIPT_NAME,
-                            NUM_POW_FLOAT_SCRIPT_NAME, NUM_TRUEDIV_SCRIPT_NAME, run_sha)
+from rapyer.scripts import (
+    NUM_FLOORDIV_SCRIPT_NAME,
+    NUM_MOD_SCRIPT_NAME,
+    NUM_MUL_SCRIPT_NAME,
+    NUM_POW_FLOAT_SCRIPT_NAME,
+    NUM_TRUEDIV_SCRIPT_NAME,
+    run_sha,
+)
 from rapyer.types.base import marks_redis_updated, RedisType
 from redis.commands.search.field import NumericField
 
