f8f8aa44c82d68f150ba85d3cfc18be261a8141d
YedidyaHKfir <Yedidya@dono.ai>
2026-06-02 12:49:41 +0300

    [242-implement-atomic-aget_or_create-classmethod-on-atomicredismodel] - avoid redis limit of push size

--- rapyer/scripts/lua/sf/redis_set/save.lua
+++ rapyer/scripts/lua/sf/redis_set/save.lua
@@ -2,8 +2,8 @@ function(special_key, payload)
     redis.call('DEL', special_key)
     if payload and payload ~= '' then
         local members = cjson.decode(payload)
-        if #members > 0 then
-            redis.call('SADD', special_key, unpack(members))
+        for i = 1, #members, 4000 do
+            redis.call('SADD', special_key, unpack(members, i, math.min(i + 3999, #members)))
         end
     end
 end
